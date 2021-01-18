04dd52491f333b7ebc356354a48db067ba5f4697
Ale <alessandrochb@gmail.com>
2021-01-18 17:02:54 +0100

    inserito try & catch per controllare i construct delle classi

--- index.php
+++ index.php
@@ -10,53 +10,63 @@ echo "<strong>Utente 'base'</strong> ";
 echo '<br>';
 
 try {
-    $userBaseOne = new User ('M', 'Rossi');
+    $userBaseOne = new User ('Mario', 'Rossi');
     echo 'Nome : ' . $userBaseOne->getName();
+    echo '<br>';
+    echo 'Cognome : ' . $userBaseOne->getLastname();
+    echo '<br>';
+
+    // stampo messaggio di errore se l'età non è corretta con try e chatch
+    try {
+        $userBaseOne->setAge(50);
+        echo 'Anni : ' . $userBaseOne->getAge();
+    } catch (Exception $e) {
+        echo 'ERRORE : ' . $e->getMessage();
+    }
+
+    echo '<br>';
+    $userBaseOne->setPoint(10);
+    echo 'Punti per blog : ' . $userBaseOne->getPoint();
+
 } catch (Exception $e) {
     echo 'ERRORE : ' . $e->getMessage();
 }
 
-
-echo '<br>';
-
-// stampo messaggio di errore se l'età non è corretta con try e chatch
-try {
-    $userBaseOne->setAge(50);
-    echo 'Anni : ' . $userBaseOne->getAge();
-} catch (Exception $e) {
-    echo 'ERRORE : ' . $e->getMessage();
-}
-
-echo '<br>';
-$userBaseOne->setPoint(10);
-echo 'Punti per blog : ' . $userBaseOne->getPoint();
 echo '<br>';
 echo '<br>';
-
 
 // utente "advanced"
 echo "<strong> Utente 'advanced' </strong>";
 echo '<br>';
-$userAdvOne = new UserAdvanced ('Pippo', 'Bianchi');
-echo 'Nome : ' . $userAdvOne->getName();
-echo '<br>';
-
-// stampo messaggio di errore se l'età non è corretta con try e chatch
 try {
-    $userAdvOne->setAge(35);
-    echo 'Anni : ' . $userAdvOne->getAge();
+    $userAdvOne = new UserAdvanced ('Pippo', 'Bianchi');
+    echo 'Nome : ' . $userAdvOne->getName();
+    echo '<br>';
+    echo 'Cognome : ' . $userAdvOne->getLastname();
+    echo '<br>';
+
+    // stampo messaggio di errore se l'età non è corretta con try e chatch
+    try {
+        $userAdvOne->setAge(35);
+        echo 'Anni : ' . $userAdvOne->getAge();
+    } catch (Exception $e) {
+        echo 'ERRORE : ' . $e->getMessage();
+    }
+
+    echo '<br>';
+    $userAdvOne->setCity('Milano');
+    echo 'Città : ' . $userAdvOne->getCity();
+    echo '<br>';
+    $userAdvOne->setJob('Actor');
+    echo 'Lavoro : ' . $userAdvOne->getJob();
+    echo '<br>';
+    $userAdvOne->setPoint(10);
+    echo 'Punti per blog : ' . $userAdvOne->getPoint();
+    echo '<br>';
+
 } catch (Exception $e) {
     echo 'ERRORE : ' . $e->getMessage();
 }
 
-echo '<br>';
-$userAdvOne->setCity('Milano');
-echo 'Città : ' . $userAdvOne->getCity();
-echo '<br>';
-$userAdvOne->setJob('Actor');
-echo 'Lavoro : ' . $userAdvOne->getJob();
-echo '<br>';
-$userAdvOne->setPoint(10);
-echo 'Punti per blog : ' . $userAdvOne->getPoint();
-echo '<br>';
+
 ?>
